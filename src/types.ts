71286f7ca8787773016594112967bb9b82ce4c51
Maxondria <maxtayebwa@gmail.com>
2020-11-21 00:28:27 +0300

    Update types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,12 @@
 import { EntityManager } from "@mikro-orm/core";
+import { Request, Response } from "express";
+
+type Session = {
+  userId?: number;
+};
 
 export type MyContext = {
   em: EntityManager<any> & EntityManager;
+  req: Request & { session: Session };
+  res: Response;
 };
