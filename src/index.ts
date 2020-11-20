3329b67396352bfb19150c83c9a2776010bb0928
Maxondria <maxtayebwa@gmail.com>
2020-11-21 00:29:31 +0300

    setup express session and redis

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,14 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/User";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { __prod__, COOKIE_NAME, SESSION_SECRET, TEN_YEARS } from "./constants";
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
 
 const main = async () => {
   const orm = await MikroORM.init(MikroORMConfig);
@@ -13,12 +21,31 @@ const main = async () => {
 
   const app = express();
 
+  app.use(
+    session({
+      name: COOKIE_NAME,
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: TEN_YEARS,
+        httpOnly: true,
+        secure: __prod__,
+        sameSite: "lax",
+      },
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver],
+      resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
