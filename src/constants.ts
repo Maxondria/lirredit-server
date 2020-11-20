e7e7ba520e2cb21abcf64a6bbe7e003e41da4cba
Maxondria <maxtayebwa@gmail.com>
2020-11-21 00:29:50 +0300

    how about we update our constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const __prod__ = process.env.NODE_ENV === "production";
 export const COOKIE_NAME = "qid";
+export const SESSION_SECRET = "someWieredSecret";
+export const TEN_YEARS = 1000 * 60 * 60 * 24 * 365 * 10;
 export const FORGET_PASSWORD_PREFIX = "forget-password:";
